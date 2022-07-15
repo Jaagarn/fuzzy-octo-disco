2e7f17c8cbce12ecfd6ff7693f2f0662cfe27a7f
Jaagarn <skoglundjesper@hotmail.com>
2022-07-15 17:34:28 +0200

    Added controls screen

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -7,6 +7,21 @@ public class PauseMenuController : MonoBehaviour
     [SerializeField]
     private GameObject pauseMenuUI;
 
+    [SerializeField]
+    private GameObject resumeButton;
+
+    [SerializeField]
+    private GameObject controlsButton;
+
+    [SerializeField]
+    private GameObject exitButton;
+
+    [SerializeField]
+    private GameObject backButton;
+
+    [SerializeField]
+    private GameObject controlsText;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -14,33 +29,41 @@ public class PauseMenuController : MonoBehaviour
             if (gameIsPaused)
                 Resume();
             else
+            {
                 Pause();
+            }
         }
     }
-
-
 
     public void OnResumeButtonClick()
     {
         Resume();
     }
 
-    public void OnMainManuButtonClick()
+    public void OnControlsButtonClick()
     {
-        // TODO: Fix a main menu
-        Debug.Log("Main menu button pressed");
+        ChangeUIScreen(changeToStarting: false);
     }
 
-    public void OnSettingsButtonClick()
+    public void OnBackButtonClick()
     {
-        // TODO: Fix Settings menu
-        Debug.Log("Settings button pressed");
+        ChangeUIScreen(changeToStarting: true);
     }
 
     public void OnExitButtonClick()
     {
         // Does not work in the editor, only on a compiled version
         Application.Quit();
+    }
+
+    private void ChangeUIScreen(bool changeToStarting)
+    {
+        resumeButton.SetActive(changeToStarting);
+        controlsButton.SetActive(changeToStarting);
+        exitButton.SetActive(changeToStarting);
+
+        backButton.SetActive(!changeToStarting);
+        controlsText.SetActive(!changeToStarting);
     }
 
     private void Resume()
